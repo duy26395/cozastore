9edd6d007cb05955daa407b9be59ca551f69c79f
Duy Nguyễn <duy26395@gmail.com>
2021-11-05 13:36:58 +0700

    sourcecoza_05112021

--- Payment-onl/config.php
+++ Payment-onl/config.php
@@ -3,4 +3,5 @@ define('URL_API','https://www.nganluong.vn/checkout.api.nganluong.post.php'); //
 define('RECEIVER','toidayhoc@datdia.com'); // Email tài khoản ngân lượng
 define('MERCHANT_ID', '56168'); // Mã merchant kết nối
 define('MERCHANT_PASS', '9cf9286010533f0af23cdd10be9b4400'); // Mật khẩu kết nôi
+$return_url = 'http://localhost/cozastore/Payment-success.php';
 ?>
